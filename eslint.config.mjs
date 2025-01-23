75b98f915fb082b0d500d097db9baafcb6522bc5
Drew Cook <drew@dco.dev>
2025-01-23 13:04:29 -0700

    chore: add in prettier, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+		plugins: ['simple-import-sort', 'prettier'],
+		rules: {
+			'prettier/prettier': 'warn',
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'warn',
+		},
+	}),
+]
 
-export default eslintConfig;
+export default eslintConfig
