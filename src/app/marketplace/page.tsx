cbef2d7582c56df8e0ac4b98612355a76e7c5495
Drew Cook <drew@dco.dev>
2025-02-17 23:43:57 -0700

    feat: marketplace page subtext

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -7,7 +7,7 @@ export default function ArboretumPage() {
 		<Page
 			metaTitle="Welcome to the Arboretum"
 			pageTitle="Welcome to the Arboretum"
-			subtext="Mint an NFT of one of the projects."
+			subtext="Explore the artist arboretum for unique music and audio NFTs, buy, sell, and trade with others, all right here on the Arbor Audio platform."
 		>
 			<NoData resource="song" tagline="Mint an NFT of one of the projects." />
 		</Page>
